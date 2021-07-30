88a518a1bac00eb19bcca4b95feae53b9276ccc1
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-30 11:01:10 +0800

    all pages and functions

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "Ulya",
+        mongodb_password: "xxxxxxxx",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "nextjs-blog",
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: "Ulya",
+      mongodb_password: "xxxxxxxx",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "nextjs-blog",
+    },
+  };
+};
