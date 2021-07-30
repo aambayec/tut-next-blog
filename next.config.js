457799c5f3630d3c29f126a64d2b6c84695ad851
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-30 14:10:20 +0800

    remove yarn file

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: "Ulya",
-        mongodb_password: "Ulya+9999",
-        mongodb_clustername: "cluster0",
-        mongodb_database: "nextjs-blog",
-      },
-    };
-  }
-
   return {
     env: {
       mongodb_username: "Ulya",
-      mongodb_password: "Ulya+9999",
+      mongodb_password: "xxxxxxxxxxx",
       mongodb_clustername: "cluster0",
       mongodb_database: "nextjs-blog",
     },
